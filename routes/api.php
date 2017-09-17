918e0e8e3191aa9bb11f589935661d607d7bb82a
saibuerns <saibuerns@github.com>
2017-09-17 19:08:39 -0300

    saque las rutas para crear roles y habilidades.

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,6 @@ Route::group(['prefix' => 'v1'], function () {
     Route::group(['prefix' => 'auth'], function () {
         Route::post('users/login', 'auth\\UserController@login');
         Route::resource('users', 'auth\\UserController', ['only' => ['index', 'store', 'show']]);
-        Route::resource('rols', 'auth\\RolController', ['only' => ['index', 'store', 'show']]);
-        Route::resource('abilities', 'auth\\AbilityController', ['only' => ['index', 'store', 'show']]);
     });
     Route::group(['prefix' => 'events'], function () {
         Route::resource('types', 'EventTypeController', ['only' => ['index', 'store', 'show']]);
